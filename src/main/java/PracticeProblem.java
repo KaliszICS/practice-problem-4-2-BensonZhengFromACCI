043a15e5fc730e3ec848d6c53d2469cc3211b7eb
BensonZhengFromACCI <343804126@tdsb.ca>
2026-04-13 15:37:51 +0000

    hey!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,40 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static String repeatLetter(char letter, int num) {
+		String result = "";
+		for (int num2 = 0; num2 != num; num2++) { 
+			result += letter;
+		}
+	return result; 
+	}
 
+	public static int countLetter(String word, char letter) {
+		int result = 0;
+		for (int pos0 = 0; pos0 != word.length(); pos0++) { 
+			if (word.charAt(pos0) == letter) {
+				result++;
+			}
+		}
+	return result; 
+	}
+
+	public static int evenSum(int num1, int num2) {
+		int result = 0;
+		if (num1 <= num2) {
+			for (int num3 = num1; num3 <= num2; num3++) {
+				if (num3 % 2 == 0) {
+					result += num3;
+				}
+			}
+		}
+		else {
+			for (int num3 = num2; num3 <= num1; num3++) {
+				if (num3 % 2 == 0) {
+					result += num3;
+				}
+			}
+		}
+	return result; 
+	}
 }
